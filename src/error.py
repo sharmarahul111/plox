a7d80b9d7a1819e5fa20cb7512449df8dcfd3cc4
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-03-13 14:37:36 +0545

    ParseError class

--- src/error.py
+++ src/error.py
@@ -2,3 +2,8 @@ class LoxRuntimeError(Exception):
 	def __init__(self, token: Token, message: str):
 		super().__init__(message)
 		self.token = token
+
+class ParseError(Exception):
+	def __init__(self, token: Token, message: str):
+		super().__init__(message)
+		self.token = token
