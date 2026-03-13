003492c5c0650009ff30fc6dc5c26a1d446109ae
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-03-13 14:39:44 +0545

    statement classs with visitors

--- src/stmt.py
+++ src/stmt.py
@@ -1,24 +1,26 @@
-class Visitor:
+from expr import Expr
+
+class StmtVisitor:
 	def visit_print_stmt(self, expr):
 		...
 	def visit_expression_stmt(self, expr):
 		...
 
 
-class Stmt:
+class Stmt():
 	def accept(self):
 		pass
 
 class Expression(Stmt):
-	def __init__(self, expr: Expr):
-		self.expr = expr
+	def __init__(self, expression: Expr):
+		self.expression = expression
 
 	def accept(self, visitor: Visitor):
-		return visitor.visit_expression_stmt()
+		return visitor.visit_expression_stmt(self)
 
 class Print(Stmt):
-	def __init__(self, expr: Expr):
-		self.expr = expr
+	def __init__(self, expression: Expr):
+		self.expression = expression
 
 	def accept(self, visitor: Visitor):
-		return visitor.visit_print_stmt()
+		return visitor.visit_print_stmt(self)
