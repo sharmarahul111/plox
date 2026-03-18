1dc712cccbac82103615798b32f7a4a41236b3a9
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-03-18 19:07:46 +0545

    resolve variable and assignment expressions

--- src/resolver.py
+++ src/resolver.py
@@ -2,7 +2,7 @@ from stmt import *
 from expr import *
 
 class Resolver(StmtVisitor, ExprVisitor):
-	def __init__(self, interpreter: Interpreter):
+	def __init__(self, lox: Lox, interpreter: Interpreter):
 		self.interpreter = interpreter
 		self.scopes: list[dict] = [] #stack
 	
@@ -17,10 +17,22 @@ class Resolver(StmtVisitor, ExprVisitor):
 			self.resolve(stmt.initializer)
 		self.define(stmt.name)
 
+	def visit_variable_expr(self, expr: Variable):
+		if len(self.scopes) != 0 and self.scopes[-1].get(expr.name.lexeme) is False:
+			self.lox.error(expr.name, "Can't read local variable in its own initializer.")
+		self.resolve_local(expr, expr.name)
+
+	def visit_assign_expr(self, expr: Assign):
+		self.resolves(expr.value)
+		self.resolve_local(expr, expr.name)
+
 	def resolve(self, statements: list):
 		for statement in statements:
 			# could be either expressions or statements
-			statement.accept(self)
+			self.resolves(statement)
+
+	def resolves(self, statement):
+		statement.accept(self)
 
 	def begin_scope(self):
 		self.scopes.append({})
@@ -39,3 +51,8 @@ class Resolver(StmtVisitor, ExprVisitor):
 			return 0
 		# is now initialized
 		self.scopes[-1][name.lexeme] = True
+
+	def resolve_local(self, expr: Expr, name: Token):
+		for i in range(len(self.scopes), 0, -1):
+			if self.scopes[i].get(name.lexeme) is not None:
+				self.interpreter.resolve(expr, len(self.scopes)-1-i)
